bded1e8074eabadb33214cf12f1ed5487c5f2146
Muhammad Uzair <101917043+Bukhariu33@users.noreply.github.com>
2024-07-30 14:13:19 +0500

    added the second screen here

---
+++ src/screens/secondScreens.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function SecondScreens() {
+  return (
+    <View>
+      <Text>
+        this is a second screen here
+      </Text>
+    </View>
+  )
+}
